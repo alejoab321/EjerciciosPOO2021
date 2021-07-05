7bc943b21e75dcde660e18498961e71d7840d396
Alejandro <alejoab1020@gmail.com>
2021-07-05 11:05:39 -0500

    Se va aclar el concepto de encapsular

--- Ejercicio01/Models/Factory.cs
+++ Ejercicio01/Models/Factory.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Ejercicio01.Models
 {
-    class Factory
+    public class Factory
     {
+        
+        public List<Car> Cars { get; set; }
+        public string Mark { get; set; }
+        public double TotalSales { get; set; }
+        public readonly int AbilityCreateCar;
+
+        public Factory(int abilityCreateCar)
+        {
+            AbilityCreateCar = abilityCreateCar;
+        }
+
+        public void CreateCar(Car car) 
+        {
+            if (AbilityCreateCar >= Cars.Count )
+            {
+                Cars.Add(car);
+                Console.WriteLine("El carro se creó satisfactoriamente");
+            }
+            else
+            {
+                Console.WriteLine("El carro no se pudo crear porque superó la capacidad de producción");
+            }
+            
+        }
+
+        public void SeeCarAll()
+        {
+            foreach (var car in Cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
     }
 }
--- Ejercicio01/Program.cs
+++ Ejercicio01/Program.cs
@@ -9,6 +9,12 @@ namespace Ejercicio01
     {
         static void Main(string[] args)
         {
+            Factory factory = new Factory(10)
+            {
+                Cars = new List<Car>(),
+                Mark = "Chevrolet",
+                TotalSales = 0
+            };
             Car car1 = new Car()
             {
                 Color = "Blue",
@@ -28,32 +34,11 @@ namespace Ejercicio01
                 Model = 2018,
                 Mark = "Chevrolet"
             };
-            List<Car> cars = new List<Car>();
-            if (!cars.Any()) //! Negación : Si cars.Any() = true por negación es false, y si es false entonces es true
-            {
-                Console.WriteLine("La lista no tiene carros");
-            }
-            cars.Add(car1);
-            cars.Add(car2);
 
-            if (cars.Any())
-            {
-                Console.WriteLine("La lista contiene carros");
-            }
-            foreach (var car in cars)
-            {
-                if (car.Model > 2018)
-                {
-                    Console.WriteLine("Carros Modelos MAyores a 2018 \n {0}" ,car);
-                    Console.WriteLine("==================================================");
-                }
-                else
-                {
-                    Console.WriteLine("Carros Modelos Menores a 2018 \n {0}", car);
-                    Console.WriteLine("==================================================");
-                }
+            factory.CreateCar(car1);
+            factory.CreateCar(car2);
 
-            }
+            factory.SeeCarAll();
 
             //Car car1 = new Car("Red",2015,"Mazda");
 
