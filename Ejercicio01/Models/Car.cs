2afc3f976941cd22f74f1adfbdc9c435c05c0c56
Alejandro <alejoab1020@gmail.com>
2021-07-05 10:55:21 -0500

    Conceptos básicos de abstracción

---
+++ Ejercicio01/Models/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01.Models
+{
+    public class Car
+    {
+        public string IdPlate { get; set; }
+        public string Color { get; set; }
+        public double Speed { get; private set; }
+        public int NumberDoors { get; set; }
+        public int Price { get; set; }
+        public string Mark { get; set; }
+        public int Model { get; set; }
+        public double SpeedUp()
+        {
+            Speed = Speed + 10;
+            return Speed;
+        }
+        public double BrakeCar()
+        {
+            Speed = Speed - 10;
+            return Speed;
+        }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string color, int model, string mark)
+        {
+            Color = color;
+            Model = model;
+            Mark = mark;
+        }
+
+        public Car(string idPlate, string color, double speed, int numberDoors, int price, string mark, int model)
+        {
+            IdPlate = idPlate;
+            Color = color;
+            Speed = speed;
+            NumberDoors = numberDoors;
+            Price = price;
+            Mark = mark;
+            Model = model;
+        }
+
+        public override string ToString()
+        {
+            return $"Mark = {Mark} \n IdPlate = {IdPlate} \n Color = {Color} \n Speed = {Speed}";
+        }
+
+    }
+}
---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,68 @@
+﻿using Ejercicio01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car()
+            {
+                Color = "Blue",
+                Price = 5000,
+                IdPlate = "KWH-125",
+                NumberDoors = 4,
+                Model = 2020,
+                Mark = "Chevrolet"
+            };
+
+            Car car2 = new Car()
+            {
+                Color = "Blue",
+                Price = 4000,
+                IdPlate = "KWH-155",
+                NumberDoors = 4,
+                Model = 2018,
+                Mark = "Chevrolet"
+            };
+            List<Car> cars = new List<Car>();
+            if (!cars.Any()) //! Negación : Si cars.Any() = true por negación es false, y si es false entonces es true
+            {
+                Console.WriteLine("La lista no tiene carros");
+            }
+            cars.Add(car1);
+            cars.Add(car2);
+
+            if (cars.Any())
+            {
+                Console.WriteLine("La lista contiene carros");
+            }
+            foreach (var car in cars)
+            {
+                if (car.Model > 2018)
+                {
+                    Console.WriteLine("Carros Modelos MAyores a 2018 \n {0}" ,car);
+                    Console.WriteLine("==================================================");
+                }
+                else
+                {
+                    Console.WriteLine("Carros Modelos Menores a 2018 \n {0}", car);
+                    Console.WriteLine("==================================================");
+                }
+
+            }
+
+            //Car car1 = new Car("Red",2015,"Mazda");
+
+            //Console.WriteLine(car1);
+            //car1.SpeedUp();
+            //Console.WriteLine(car1);
+            //car1.BrakeCar();
+            //Console.WriteLine(car1);
+            Console.Read();
+        }
+    }
+}
